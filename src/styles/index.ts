1e3fd539fef67d49e3aac6bc1a9a49d8ac112d34
savioramon <savioramon12@gmail.com>
2022-02-17 16:23:34 -0300

    definindo temas

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+
+
+export const darkTheme = {
+    backgroundColor: "#03081a",
+    textColor: "#F2F2F2",
+};
+
+
+export const lightTheme = {
+    backgroundColor: "#FFF",
+    textColor: "rgba(0, 0, 0, 0.9)",
+};
