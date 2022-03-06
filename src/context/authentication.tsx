472dcc29209759af6c23bb72c79e705d9ae6d29d
savioramon <savioramon12@gmail.com>
2022-03-06 16:43:06 -0300

    adicionando hooks para gerenciamento de dados do usuário

--- src/context/authentication.tsx
+++ src/context/authentication.tsx
@@ -1,8 +1,11 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 type Authentication = {
     authWithService: (value: string) => void;
+    setName: React.Dispatch<React.SetStateAction<string | null>>;
+    setEmail: React.Dispatch<React.SetStateAction<string | null>>;
+    setPassword: React.Dispatch<React.SetStateAction<string | null>>;
 };
 
 type AuthProps = {
@@ -14,7 +17,11 @@ export  const AuthContext = createContext({} as Authentication);
 
 
 export function AuthProvider({children}: AuthProps) {
-    
+
+    const [name, setName] = useState<string | null>("");
+    const [email, setEmail] = useState<string | null>("");
+    const [password, setPassword] = useState<string | null>("");
+
     function authWithService(value: string) {
 
         const authenticationServices = {
@@ -30,7 +37,10 @@ export function AuthProvider({children}: AuthProps) {
 
     return (
         <AuthContext.Provider value={{
-            authWithService
+            authWithService,
+            setName,
+            setEmail,
+            setPassword,
         }}>
             {children}
         </AuthContext.Provider>
