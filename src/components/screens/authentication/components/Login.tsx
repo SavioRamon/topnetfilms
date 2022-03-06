13a84b904f388a532937e1a98f165336a5e832db
savioramon <savioramon12@gmail.com>
2022-03-06 16:42:30 -0300

    removendo arquivo desnecessário

--- src/components/screens/authentication/components/Login.tsx
+++ src/components/screens/authentication/components/Login.tsx
@@ -1,19 +1,34 @@
 import styled from "styled-components";
 import metrics from "../../../../styles/metrics";
-import FormAuthContainer from "./FormAuthContainer";
+import BottomContent from "./BottomContent";
+
 
 const LoginScreen = styled.section`
-    padding: ${metrics.mediumSpacingSize} 0;
-    width: 100%;
+    padding: ${metrics.extraSmallSpacingSize};
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    border: 1px solid #999;
+`;
+
+const Title = styled.h1`
+    margin-bottom: ${metrics.extraSmallSpacingSize};
+    font-size: calc(1rem + 1vw);
+`;
+
+const FormAuthentication = styled.form`
+
 `;
 
 export default function Login() {
     return (
         <LoginScreen>
-            <FormAuthContainer />
+            <Title>Login</Title>
+
+            <FormAuthentication>
+                <BottomContent />
+            </FormAuthentication>
         </LoginScreen>
     );
 };
--- src/components/screens/authentication/components/Register.tsx
+++ src/components/screens/authentication/components/Register.tsx
@@ -1,19 +1,34 @@
 import styled from "styled-components";
 import metrics from "../../../../styles/metrics";
-import FormAuthContainer from "./FormAuthContainer";
+import BottomContent from "./BottomContent";
+
 
 const RegisterScreen = styled.section`
-    padding: ${metrics.mediumSpacingSize} 0;
-    width: 100%;
+    padding: ${metrics.extraSmallSpacingSize};
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    border: 1px solid #999;
+`;
+
+const Title = styled.h1`
+    margin-bottom: ${metrics.extraSmallSpacingSize};
+    font-size: calc(1rem + 1vw);
+`;
+
+const FormAuthentication = styled.form`
+
 `;
 
 export default function Register() {
     return (
         <RegisterScreen>
-            <FormAuthContainer />
+            <Title>Create account</Title>
+            
+            <FormAuthentication>
+                <BottomContent />
+            </FormAuthentication>
         </RegisterScreen>
     );
 };
