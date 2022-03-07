084c32765d554f5d62f3a32c3bc975c78d1137e6
savioramon <savioramon12@gmail.com>
2022-03-07 19:15:18 -0300

    estilizando inputs de autenticação

--- src/components/screens/authentication/components/Input.tsx
+++ src/components/screens/authentication/components/Input.tsx
@@ -1,33 +1,60 @@
 import styled from "styled-components";
+import metrics from "../../../../styles/metrics";
 
 const InputContent = styled.fieldset`
+    width: 100%;
+    margin-bottom: ${metrics.smallSpacingSize};
+    border: none;
+    display: flex;
+`;
 
+const LabelArea = styled.label`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    border-bottom: .1vw solid #999;
 `;
 
 const InputIcon = styled.div`
-
+    padding: ${metrics.extraSmallSpacingSize};
+    font-size: calc(.9rem + .9vw);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
 `;
 
 const InputElement = styled.input`
-
+    width: 100%;
+    border: none;
+    outline: none;
+    font-size: calc(.9rem + .9vw);
 `;
 
 type InputProps = {
+    id: string;
     type: string;
     placeholder: string;
     icon: JSX.Element;
     setValue: (value: string)=>void
 };
 
-const Input = ({ type, placeholder, icon, setValue }: InputProps) => {
+const Input = (props: InputProps) => {
     return (
         <InputContent>
-            {icon}
-            <InputElement
-                type={type} 
-                placeholder={placeholder} 
-                onChange={e => setValue(e.target.value)}
-            />
+            <LabelArea htmlFor={props.id}>
+                <InputIcon>
+                    {props.icon}
+                </InputIcon>
+
+                <InputElement
+                    id={props.id}
+                    type={props.type} 
+                    placeholder={props.placeholder} 
+                    onChange={e => props.setValue(e.target.value)}
+                />
+            </LabelArea>
         </InputContent>
     );
 };
