d88038f0e06ab8ecaeee518a790d201f0985affb
savioramon <savioramon12@gmail.com>
2022-03-07 19:55:06 -0300

    alterando cor de temas

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,12 +1,12 @@
 
 
 export const darkTheme = {
-    backgroundColor: "#00020a",
-    textColor: "#F2F2F2",
+    backgroundColor: "#1c1c1c",
+    textColor: "#fff",
 };
 
 
 export const lightTheme = {
     backgroundColor: "#FFF",
-    textColor: "rgba(0, 0, 0, 0.9)",
+    textColor: "#262626",
 };
