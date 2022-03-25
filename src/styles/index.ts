f234b14937853a19b3fe679d9b1aa88dfee3dcb6
savioramon <savioramon12@gmail.com>
2022-03-25 16:22:50 -0300

    adicionando cor de fundo secundária para temas

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,16 +1,19 @@
 
 export type ThemePropsTypes = {
     backgroundColor: string;
+    backgroundSecondary: string;
     textColor: string;
 }
 
 export const darkTheme: ThemePropsTypes = {
     backgroundColor: "#1c1c1c",
+    backgroundSecondary: "#2e2e2e",
     textColor: "#fff",
 };
 
 
 export const lightTheme: ThemePropsTypes = {
-    backgroundColor: "#FFF",
+    backgroundColor: "#c9c9c9",
+    backgroundSecondary: "#FFF",
     textColor: "#262626",
 };
