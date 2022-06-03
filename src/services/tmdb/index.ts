465d7ac002231f2e481efd3b9bab1e68b332a493
savioramon <savioramon12@gmail.com>
2022-06-03 14:07:05 -0300

    idioma da api em inglês

--- src/services/tmdb/index.ts
+++ src/services/tmdb/index.ts
@@ -1,6 +1,6 @@
 const BASE_API = "https://api.themoviedb.org/3";
 const API_KEY = "3783a8ddb11beaabb09ac6d2dad893f8";
-const otherInfos = `language=pt-BR&api_key=${API_KEY}`;
+const otherInfos = `language=en&api_key=${API_KEY}`;
 
 const basicFetch = async (reqDetails: string) => {
     const data = await fetch(`${BASE_API}/${reqDetails}&${otherInfos}`);
@@ -15,7 +15,7 @@ export const tmdbGetHomeListData = async () => {
             data: await basicFetch("movie/popular?")
         },
         {
-            title: "Tendências",
+            title: "Trending",
             data: await basicFetch("trending/movie/day?")
         }
     ];
