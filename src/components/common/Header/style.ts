ec536b013d27035a9930c62a3e5e7e5cd510f17e
savioramon <savioramon12@gmail.com>
2022-06-03 21:05:41 -0300

    cabeçalho fixo

--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -2,11 +2,13 @@ import styled from "styled-components";
 import metrics from "../../../styles/metrics";
 
 export const HeaderContainer = styled.header`
-    padding: ${metrics.extraSmallSpacingSize};
+    z-index: 10;
+    padding: ${metrics.extraSmallSpacingSize} ${metrics.mediumSpacingSize};
     width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
     background-color: ${({theme})=>theme.backgroundSecondary};
     border-bottom: .1vw solid #333;
+    position: fixed;
 `;
