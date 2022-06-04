b5fd4ce8f747d73cefaf427d8ef7479d05855281
savioramon <savioramon12@gmail.com>
2022-06-03 21:08:28 -0300

    adicionando ícones nas ações de login de usuário

--- src/components/common/Header/components/ConfigMenu/components/BottomConfig.tsx
+++ src/components/common/Header/components/ConfigMenu/components/BottomConfig.tsx
@@ -4,22 +4,27 @@ import styled from "styled-components";
 import { useAuthentication } from "../../../../../../hooks";
 import metrics from "../../../../../../styles/metrics";
 
+import { BiLogOut, BiLogIn, BiUser } from "react-icons/bi";
+
 
 const Content = styled.div`
-    margin-top: ${metrics.mediumSpacingSize};
+    margin-top: ${metrics.extraSmallSpacingSize};
     padding: ${metrics.extraSmallSpacingSize} 0;
     width: 100%;
     border-top: .1vw solid rgba(100, 100, 100, 0.3);
 `;
 
 const AuthButton = styled.button`
-    padding: .3em 0;
+    padding: .5em 0 .5em 1em;
     width: 100%;
     font-size: 1em;
+    text-indent: .6em;
     border: none;
     cursor: pointer;
     color: ${({theme})=>theme.textColor};
     background-color: inherit;
+    display: flex;
+    align-items: center;
 
     &:hover {
         background-color: rgba(140, 140, 140, 0.2);
@@ -44,18 +49,22 @@ const BottomConfig = ({changeConfigDisplay}: Props):JSX.Element => {
         <Content>
             {userData?
                 <AuthButton onClick={disconnecting}>
-                    Sign out
+                    <BiLogOut /> Log Out
                 </AuthButton>
 
                 :
 
                 <Fragment>
                     <Link to="login">
-                        <AuthButton onClick={changeConfigDisplay}>Login</AuthButton>
+                        <AuthButton onClick={changeConfigDisplay}>
+                            <BiLogIn /> Login
+                        </AuthButton>
                     </Link>
 
                     <Link to="register">
-                        <AuthButton onClick={changeConfigDisplay}>Sign in</AuthButton>
+                        <AuthButton onClick={changeConfigDisplay}>
+                            <BiUser /> Sign Up
+                        </AuthButton>
                     </Link>
                 </Fragment>
             }
