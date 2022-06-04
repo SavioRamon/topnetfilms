dd7be54f9ce566ec8a9b18ef1ef99aca92dd3849
savioramon <savioramon12@gmail.com>
2022-06-03 21:07:55 -0300

    ajuste no espaçamento pequeno

--- src/styles/metrics.ts
+++ src/styles/metrics.ts
@@ -1,7 +1,7 @@
 
 
 export default {
-    extraSmallSpacingSize: "calc(.5rem + .5vw)",
+    extraSmallSpacingSize: "calc(.3rem + .3vw)",
     smallSpacingSize: "calc(1rem + 1vw)",
     mediumSpacingSize: "calc(1.5rem + 1.5vw)",
     largeSpacingSize: "calc(3rem + 3vw)"
