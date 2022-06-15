b81493cb508253ecf4ff46beb8f955ca8de5e39f
savioramon <savioramon12@gmail.com>
2022-06-15 17:19:59 -0300

    adicionando constantes de rotas

--- src/utils/CONSTANTS.ts
+++ src/utils/CONSTANTS.ts
@@ -1,9 +1,15 @@
-
+const ROUTES = {
+    LOGIN: "login",
+    REGISTER: "register",
+    SEARCH: "search",
+    FILM: "film"
+}
 
 const PARAMS = {
     GOOGLE: "GOOGLE"
 };
 
 export default {
-    PARAMS
+    PARAMS,
+    ROUTES
 };
