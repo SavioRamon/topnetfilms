6c11f343286307872533a047a531ebc6d1315fe9
savioramon <savioramon12@gmail.com>
2022-06-16 16:39:32 -0300

    renderizando imagem de filme

--- src/components/screens/Film/index.tsx
+++ src/components/screens/Film/index.tsx
@@ -1,13 +1,12 @@
-import { useLayoutEffect } from "react";
+import { Fragment, useLayoutEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useMovieList } from "../../../hooks";
-import { Content } from "./style";
-
+import { Content, FilmInformations, LargeImage } from "./style";
 
 
 export default function Film() {
 
-    const { getSingleFilm } = useMovieList();
+    const { singleFilm, getSingleFilm } = useMovieList();
     const { id } = useParams();
 
     useLayoutEffect(()=>{
@@ -22,8 +21,15 @@ export default function Film() {
     }, []);
 
     return (
-        <Content>
-
-        </Content>
+        <Fragment>
+            {singleFilm &&
+                <Content>
+                    <LargeImage src={`https://image.tmdb.org/t/p/original/${singleFilm.poster_path}`} alt={singleFilm.title} />
+                    <FilmInformations>
+                        
+                    </FilmInformations>
+                </Content>
+            }
+        </Fragment>
     );
 };
--- src/components/screens/Film/style.ts
+++ src/components/screens/Film/style.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
 export const Content = styled.div`
+    padding: calc(1rem + 2vw) 0;
+    width: 100%;
+    height: min-content;
+    display: flex;
+    justify-content: center;
+    background-color: red;
+`;
 
+export const LargeImage = styled.img`
+    margin-left: calc(1rem + 1vw);
+    width: calc(5rem + 27vw);
+    height: calc(5rem + 32vw);
+
+    @media (max-width: 450px) {
+        margin-left: 0;
+        width: 95vw;
+        height: 98vw;
+    };
+`;
+
+export const FilmInformations = styled.section`
+    flex: 1;
 `;
