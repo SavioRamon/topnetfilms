0b6b2c15561e4cac82e69f197ce08f7490fc9f45
savioramon <savioramon12@gmail.com>
2022-06-29 18:35:03 -0300

    instalando e configurando plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
 import react from '@vitejs/plugin-react'
+import eslint from "vite-plugin-eslint"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    eslint()
+  ]
 })
