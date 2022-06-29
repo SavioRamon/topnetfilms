98dab8affe314a78982dbdc7092b6c27c4453d65
savioramon <savioramon12@gmail.com>
2022-06-29 13:36:35 -0300

    adicionando componente de lista de filmes na tela de pesquisa

--- src/components/screens/Search/index.tsx
+++ src/components/screens/Search/index.tsx
@@ -1,23 +1,26 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useMovieList } from "../../../hooks";
+import FilmListResults from "../../common/FilmListResults";
 import { Container } from "./style";
 
 
 
 export default function Search() {
 
-    const { getSearchResults, searchResults } = useMovieList();
+    const { getSearchResults, searchResults, loading } = useMovieList();
     const { query } = useParams();
     
 
-    useEffect(()=>{
+    useLayoutEffect(()=>{
         if(query) getSearchResults(query);
     }, []);
 
     return (
         <Container>
-            
+            {!loading && searchResults && 
+                <FilmListResults />
+            }
         </Container>
     );
 }
