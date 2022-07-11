e97ccbc8c29f459da6eb14410466bfc52c5b4c63
savioramon <savioramon12@gmail.com>
2022-07-11 14:01:40 -0300

    adding new colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,10 +3,14 @@ export type ColorTypes = {
     blue: string;
     white: string;
     header: string;
+    red: string;
+    gray: string;
 }
 
 export default {
     blue: "#4800ff",
     white: "#fff",
     header: "#1b1f2d",
+    red: "#ff0a2f",
+    gray: "#818181"
 };
