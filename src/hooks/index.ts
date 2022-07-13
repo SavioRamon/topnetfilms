446a23e7c840cb873c801bbe353efabbbda596e3
savioramon <savioramon12@gmail.com>
2022-07-13 14:48:37 -0300

    removing the authentication hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,7 @@
 import { useContext } from "react";
 
-import { AuthContext } from "../context/authentication";
 import { ThemeContext } from "../context/themeContext";
 
 
 export const useTheme = () => useContext(ThemeContext);
 
-export const useAuthentication = () => useContext(AuthContext);
