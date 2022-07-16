0c720b1bc31bbc3128334b4357cbe34a2bc76584
savioramon <savioramon12@gmail.com>
2022-07-16 14:33:09 -0300

    top side profile component

---
+++ src/components/screens/Profile/components/TopContent.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { useAppSelector } from "../../../../store/hooks";
+import UserImage from "./UserImage";
+
+const Content = styled.div`
+    padding: 1em 1em .3em 1em;
+    font-size: calc(1rem + .6vw);
+    display: flex;
+    align-items: flex-end;
+    border-bottom: .1em solid ${({theme})=>theme.gray};
+    background-color: ${({theme})=>theme.backgroundSecondary};
+`;
+
+const Text = styled.span`
+    margin-left: 1em;
+    font-weight: bold;
+`;
+
+
+const TopContent = () => {
+
+    const accountInfo = useAppSelector((state)=>state.user.accountInfo);
+
+    return (
+        <Content>
+            {accountInfo &&
+                <>
+                    <UserImage image={ accountInfo.photoURL } />
+                    <Text>{ accountInfo.displayName }</Text>
+                </>
+            }
+        </Content>
+    )
+}
+
+export default TopContent;
---
+++ src/components/screens/Profile/components/UserImage.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import images from "../../../../assets/images";
+
+
+const Image = styled.img`
+    width: calc(3rem + 6vw);
+    height: calc(3rem + 6vw);
+`;
+
+
+
+const UserImage = ({ image }: {image: string | null}) => {
+    return (
+        <Image src={image? image : images.userDefault} alt="your user image"/>
+    )
+}
+
+export default UserImage;
